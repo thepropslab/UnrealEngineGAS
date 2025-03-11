ea537ba1f8951e8a039d312b97e6b96d75a0d858
thepropslab <113956475+thepropslab@users.noreply.github.com>
2025-03-11 21:58:04 +1100

    Test commit

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext); // halt executions if this fails (game will crash)
+
+	// Setup the input subsystem for the player, connect it to the mapping context and set some basic control behaviour
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	// Need to convert input component to enhanced input component, but crash if the game can't detect this
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	// Create the callback calling the move function when the input action value is filled in
+	EnhancedInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered,this, &AAuraPlayerController::Move);	
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	// Calculate the unit vector pointing from the camera to the character, and convert to a forward vector and right vector.
+	const FVector2d InputAxisVector = InputActionValue.Get<FVector2d>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	// Take the unit vectors and scale them by the inputaxisvector sizes
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
