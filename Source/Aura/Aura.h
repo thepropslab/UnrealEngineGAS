3d8692b2ddd39fd60766c15d4bb521b564cde180
Chris <thepropslab@gmail.com>
2025-10-14 19:31:03 +1100

    custom collision channel

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -5,3 +5,5 @@
 #include "CoreMinimal.h"
 
 #define CUSTOM_DEPTH_RED 250
+// Create an easy way of referencing a custom gameplay channel
+#define ECC_Projectile ECollisionChannel::ECC_GameTraceChannel1
